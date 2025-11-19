14d8d4d9d8c2b706fde84d7738874f52c5030a1c
Sarthak Pawar <pawar.sarthak24@gmail.com>
2025-11-19 15:02:15 +0530

    done890001

--- backend_repo/index.js
+++ backend_repo/index.js
@@ -1,17 +1,27 @@
-import express, { json } from 'express'
-import { router } from './routes/index.js';
-import { userRouter } from './routes/user.js';
-import cors from 'cors'
-import { accountRouter } from './routes/account.js';
+import express from "express";
+import cors from "cors";
+import { router } from "./routes/index.js";
+import { userRouter } from "./routes/user.js";
+import { accountRouter } from "./routes/account.js";
 
-const app=express();
-app.use(cors())
-app.use(express.json())
+const app = express();
 
-app.use('/api/v1',router)
-app.use('/api/user',userRouter)
-app.use('/api/v1/account',accountRouter)
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
-app.listen(3000)
+app.use(express.json());
 
+app.use("/api/v1", router);
+app.use("/api/user", userRouter);
+app.use("/api/v1/account", accountRouter);
 
+app.get("/", (req, res) => {
+    res.send("Backend Running Successfully!");
+});
+
+// Render PORT fix
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on " + PORT));
