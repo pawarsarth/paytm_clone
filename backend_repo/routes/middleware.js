473f2a1c519c755b6e1b25a17195ede4cd64afb2
Sarthak Pawar <pawar.sarthak24@gmail.com>
2025-11-19 15:26:24 +0530

    done8900011

--- backend_repo/routes/middleware.js
+++ backend_repo/routes/middleware.js
@@ -1,29 +1,17 @@
-import jwt from 'jsonwebtoken'
-import { JWT_SECRET } from '../config.js'
+export const userMiddleware = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
 
-function userMiddleware(req,res,next)
-{
-   try{
-     const token =req.headers['token']
-
-    if(!token)
-    {
-        return res.statuts(401).json({
-            message:"token missing"
-        })
-        
+    if (!authHeader) {
+        return res.status(403).json({ msg: "No token provided" });
     }
-    const decodedData=jwt.verify(token,JWT_SECRET)
 
-        req.userId=decodedData.id
+    const token = authHeader.split(" ")[1]; // Remove "Bearer"
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.userId = decoded.userId;
         next();
-   }
-   catch(e)
-   {
-    res.json({
-        message:"toke error"
-    })
-   }
-}
-
-export {userMiddleware}
+    } catch (error) {
+        return res.status(403).json({ msg: "Invalid token" });
+    }
+};
